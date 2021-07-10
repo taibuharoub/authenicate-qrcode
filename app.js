891f54ac8991f8531ba575fad37675e7879eabe9
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-10 16:23:06 +0300

    Add error handling

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((error, req, res, next) => {
+   console.log(error);
+   const status = error.statusCode || 500;
+   const message = error.message;
+   const data = error.data;
+   res.status(status).json({ message: message, data: data });
+ });
+ 
+
 app.listen(port, () => {
   console.log(`Server Started at http://localhost:${port}`);
 });
